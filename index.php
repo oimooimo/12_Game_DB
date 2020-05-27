a44dfb0a909f68a94249b9e747e34f3eb40fcffd
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-05-27 12:36:58 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+
+<HTML lang="eng">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="games,apps,">
+    <meta name="GTT" content="Game/App Database">
+    <meta name="keywords" content="games,apps,ratings">
+
+    <title>Game Database</title>
+
+    <!-- for multiple font change | change to %7c no spaces -->
+    <link href="http://fonts.googleapis.com/css?family=Lato%7cUnbuntu" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/data-style.css"> <!-- custom style sheet-->
+
+
+</head>
+
+<body>
+
+        <p class="message">Eek! Your browser does not support grid. Please upgrade your system.</p>
+
+        <div class="wrapper">
+
+            <!-- Logo/sma;; image goeshere-->
+            <div class="box logo">
+                <a href="index.php"><img src="images/logo.png" width="261" height ="150" alt="dice"/> ,/a>
+            </div>
+
+            <div class="box banner">
+                <h1> Games Database</h1>
+            </div>
+
+            <div class= "box main">
+                <h2> welcome </h2>
+
+
+                <p>
+                Put link to original dataset here
+                </p>
+
+            </div><!--main-->
+
+            <div class="box slide">
+                <h2>Search Area</h2>
+
+                <p>This is where the search area goes</p>
+            </div> <!-- /sidebar-->
+
+            <div class="box footer">
+                CC GTT 20XX
+                </div> <!--/boxfooter-->
+        </div> <!--wrapper-->
+</body>
+</HTML>
+
