acfd949d24a3bddc207b46381f1d4138dedad2df
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-05-30 10:31:27 +1200

    Further Setup
    Created showall.php that goes to show all slide when clicked. also link in bottom bit has link for show all so that show all can show results.
    
    Adjusted some css, but didnt do much because this is a practice and I am not worried about aethetics
    
    Also fixed bottom bit because it was missing a  php bracket ending.

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 
 
                 <p>
-                Put link to original dataset here
+                This database contains information about all the latest mobile strategy games on the Apple app store. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games"> original dataset</a> can be found at the following link.
                 </p>
 
             </div><!--/main-->
 
-<?php include 'bottombit.php'         
+<?php include 'bottombit.php' ?>    
---
+++ showall.php.php
@@ -0,0 +1,12 @@
+<?php include 'topbit.php' ?>
+
+            <div class= "box main">
+                <h2> All Results </h2>
+
+
+                <p>Results will go here....
+                </p>
+
+            </div><!--/main-->
+
+<?php include 'bottombit.php' ?>        
