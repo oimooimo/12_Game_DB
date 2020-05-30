aa2c96e05cab3cb9303ba9bacb2adcabdf8588d1
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-05-30 19:25:37 +1200

    Results pt1
    pt 1 of showing results from DB
    
    adding in php queries

--- showall.php
+++ showall.php
@@ -1,11 +1,51 @@
-<?php include 'topbit.php' ?>
+<?php include ('topbit.php');
+
+    $find_sql="SELECT * FROM `game_details`";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
+
 
             <div class= "box main">
                 <h2> All Results </h2>
+                
+                <?php 
+                
+                if($count < 1) {
 
+                ?>
 
-                <p>Results will go here....
-                </p>
+                <div class="error">
+                    
+                    Sorry! There are no results that match your search. Please use the search box in the side bar to try again.
+
+                </div><!-- end error-->
+
+                <?php
+                } // end no results if
+
+                else{
+                    do
+                    {
+
+                        ?>
+
+            <!-- results go here-->
+            <div class="results">
+                You have results!
+            </div><!--/results-->
+
+            <?php
+
+                }// end results 'do'
+
+                while
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+            }// end here
+
+            ?>
 
             </div><!--/main-->
 
