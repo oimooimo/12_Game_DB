dd40a948c03736ad687e9a98e02e10d96be49a3a
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-01 21:33:32 +1200

    Added Acknowledgement and sources area
    Added said area and linked sources of original Dataset on kaggle and the original perctange based star rating code

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
             <div class= "box main">
                 <h2> welcome </h2>
 
-
                 <p>
-                This database contains information about all the latest mobile strategy games on the Apple app store. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games"> original dataset</a> can be found at the following link.
+                This database contains information about all the latest mobile strategy games on the Apple app store. 
                 </p>
 
+                <h2> Acknowledgements & Sources </h2>
+
+                <li> <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games"> Original 17K Mobile Strategy Games Dataset</a>  <li> 
+                <li> <a href="https://codepen.io/Bluetidepro/pen/GkpEa"> Original Percentage based star rating code</a>  <li> 
+
+                </p>
             </div><!--/main-->
 
-<?php include 'bottombit.php' ?>    
+<?php include 'bottombit.php' ?>        
