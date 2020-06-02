98677b7f4db4c973acc6f24d5073d61c9969a2c0
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-02 16:10:30 +1200

    Search pt1
    Creates an area for the search, however it does not work because its linked to a on-existent php page. Users will be lead to an error page if something is entered

--- bottombit.php
+++ bottombit.php
@@ -1,8 +1,15 @@
  <div class="box slide">
+
                 <h2>Add an App|
                     <a class="side" href="showall.php"> Show All</a></h2>
 
-                <p>This is where the search area goes</p>
+                <form class="searchform" method="post" action="name_dev.php" enctype="multipart/form-data">
+                
+                    <input class="search" type="text" name="dev_name" size="40" value="" required placeholder="App Nam / Developer Name..." />
+
+                    <input class="submit" type="submit" name="find_game_name" value="&#xf002;"/>
+                </form>
+
             </div> <!-- /sidebar-->
 
             <div class="box footer">
