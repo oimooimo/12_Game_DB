264ee6f3574cdb226d1492f279ada490882d13b9
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-03 19:24:25 +1200

    advanced search pt 4
    created php page for advanced search

---
+++ advanced.php
@@ -0,0 +1,32 @@
+<?php include ('topbit.php');
+
+    $app_name = mysqli_real_escape_string($dbconnect, $_POST['app_name']);
+    $developer = mysqli_real_escape_string($dbconnect, $_POST['dev_name']);
+    $genre = mysqli_real_escape_string($dbconnect, $_POST['genre']);
+    $cost = mysqli_real_escape_string($dbconnect, $_POST['cost']);
+  
+    $find_sql="SELECT * FROM `game_details`
+    JOIN genre ON (game_details.GenreID = genre.GenreID)
+    JOIN developer ON (game_details.DeveloperID = developer.DeveloperID)
+    WHERE `Name` LIKE '%$app_name%'
+    AND `DevName` LIKE '%$developer%'
+    AND `Genre`LIKE '%$genre%'
+    AND `Price` <='$cost'
+
+    ";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
+
+
+            <div class= "box main">
+                <h2> Name / Developer Name Search </h2>
+
+            <?php include 'results.php' ?>
+
+
+            </div><!--/main-->
+
+<?php include 'bottombit.php' ?>        
