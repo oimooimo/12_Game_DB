92010c40b59861a58e5d72e0bb91c455a02bd091
oimooimo <65986787+oimooimo@users.noreply.github.com>
2020-06-04 19:25:58 +1200

    removed required

--- bottombit.php
+++ bottombit.php
@@ -1,11 +1,11 @@
- <div class="box slide">
+  <div class="box slide">
 
-                  <h2><a href="addentry.php"> Add an App</a>|
+                <h2><a href="addentry.php"> Add an App</a>|
                     <a class="side" href="showall.php"> Show All</a></h2>
 
                 <form class="searchform" method="post" action="name_dev.php" enctype="multipart/form-data">
                 
-                    <input class="search" type="text" name="dev_name" size="40" value="" required placeholder="App Nam / Developer Name..." />
+                    <input class="search" type="text" name="dev_name" size="40" value=""  placeholder="App Nam / Developer Name..." />
 
                     <input class="submit" type="submit" name="find_game_name" value="&#xf002;"/>
  
@@ -60,7 +60,7 @@
 
                 <!--Cost-->
                 <div class="flex-container">
-                    <div class="adv-txt">
+                    <div class="adv-txt adv-cost">
                         Cost&nbsp;(less&nbsp;than):
                     </div> <!-- /cost lable-->
 
